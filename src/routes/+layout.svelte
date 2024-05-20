aee5b1f6cdbf2d249b6a5a3862bb955d9e86680d
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2024-05-20 16:44:10 +1000

    Add some basic WIP UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/estimations/Leader">Story Point Estimations: Leader</a>
+  <a href="/estimations/member">Story Point Estimations: Member</a>
+</nav>
+
+<div class="content-container">
+  <div class="header-container">
+    <h1>Header</h1>
+  </div>
+  <div class="content">
+    <slot />
+  </div>
+</div>
+
+<style>
+  nav {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    width: 17vw;
+    gap: 1rem;
+    padding: 1rem;
+    background: var(--bg-2);
+    border-radius: var(--border-radius);
+  }
+
+  nav a {
+    font-weight: 500;
+    text-decoration: none;
+  }
+
+  .content-container {
+    flex: 1;
+    margin-left: 1vh;
+  }
+
+  .header-container {
+    background-color: var(--bg-2);
+    border-radius: var(--border-radius);
+    flex: 1;
+    padding: 0.5rem;
+  }
+
+  .content {
+    margin-top: 1rem;
+  }
+</style>
